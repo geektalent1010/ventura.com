670626c1f6e5b73f6c582054877feb65970f6782
Zsolt Molnar <zsolt-molnar@users.noreply.github.com>
2023-12-16 12:33:07 +0200

    artisan down template (#16)

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<title>{{ config('app.name', 'Ventura') }} @yield('title')</title>
+<style>
+    h1 { font-size: 50px; }
+    body { font: 20px Helvetica, sans-serif; color: #a0aec0; text-align: center; padding: 150px; background-color: rgba(26,32,44, 1) }
+    article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+</style>
+
+<article>
+    <h1>We&rsquo;ll be back soon!</h1>
+    <div>
+        <p>Sorry for the inconvenience we&rsquo;ll be online shortly!</p>
+        <p>&mdash; Ventura Team</p>
+    </div>
+</article>
